a5fc44f3142f7b86eed20c46eb3e6b98d8b82a5b
Yan Bin Pan <yanbin.pan6@outlook.com>
2021-02-20 18:42:40 +0000

    pushing commits

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+
+# Connect to the database
+
+# Add connection to the localhost postgres db environmental variable
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
+SQLALCHEMY_TRACK_MODIFICATIONS = False
